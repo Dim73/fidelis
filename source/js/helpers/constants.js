b4a3ddc8a2fbcbf8923f843bbb51ab0a63cb0299
Dim <ebox.di@gmail.com>
2016-01-21 13:43:34 +0300

    rebuild

--- source/js/helpers/constants.js
+++
@@ -1,9 +0,0 @@
-var cookie = require('cookies-js');
-module.exports = (function(){
-  var _const =  {};
-
-  _const.ENV_CONST = window.location.host && (/^[^\:]+\:[\d]+/.test(window.location.host))?'dev':'prod';
-  _const.AJX_PATH = _const.ENV_CONST === 'dev'?'../../source/back/':'/ajax/';
-  _const.IS_MOBILE = cookie.get('show_mobile');///Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent);
-  return _const
-})()
